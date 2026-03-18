360c6a2e109df842c3a5d99b6b0ca13b93886e0d
loeredami <jskfbjsn018@gmail.com>
2026-03-19 00:14:44 +0100

    added ForEachGo

--- small_map.go
+++ small_map.go
@@ -1,36 +1,32 @@
 package ungo
 
-type entry[K comparable, V any] struct {
-	key   K
-	value V
-}
-
 type SmallMap[K comparable, V any] struct {
-	data []entry[K, V]
+	keys   []K
+	values []V
 }
 
 func NewSmallMap[K comparable, V any](capacity int) *SmallMap[K, V] {
 	return &SmallMap[K, V]{
-		data: make([]entry[K, V], 0, capacity),
+		keys:   make([]K, 0, capacity),
+		values: make([]V, 0, capacity),
 	}
 }
 
 func (fm *SmallMap[K, V]) Set(key K, value V) {
-	d := fm.data
-	for i := 0; i < len(d); i++ {
-		if d[i].key == key {
-			d[i].value = value
+	for i, k := range fm.keys {
+		if k == key {
+			fm.values[i] = value
 			return
 		}
 	}
-	fm.data = append(fm.data, entry[K, V]{key: key, value: value})
+	fm.keys = append(fm.keys, key)
+	fm.values = append(fm.values, value)
 }
 
 func (fm *SmallMap[K, V]) Get(key K) (V, bool) {
-	d := fm.data
-	for i := 0; i < len(d); i++ {
-		if d[i].key == key {
-			return d[i].value, true
+	for i, k := range fm.keys {
+		if k == key {
+			return fm.values[i], true
 		}
 	}
 	var zero V
@@ -38,59 +34,63 @@ func (fm *SmallMap[K, V]) Get(key K) (V, bool) {
 }
 
 func (fm *SmallMap[K, V]) Delete(key K) {
-	d := fm.data
-	for i := 0; i < len(d); i++ {
-		if d[i].key == key {
-			lastIdx := len(d) - 1
-			d[i] = d[lastIdx]
+	for i, k := range fm.keys {
+		if k == key {
+			lastIdx := len(fm.keys) - 1
 
-			var zero entry[K, V]
-			d[lastIdx] = zero
+			// Swap with last to maintain O(1) delete
+			fm.keys[i] = fm.keys[lastIdx]
+			fm.values[i] = fm.values[lastIdx]
 
-			fm.data = d[:lastIdx]
+			// Clear last elements to avoid memory leaks
+			var zeroK K
+			var zeroV V
+			fm.keys[lastIdx] = zeroK
+			fm.values[lastIdx] = zeroV
+
+			fm.keys = fm.keys[:lastIdx]
+			fm.values = fm.values[:lastIdx]
 			return
 		}
 	}
 }
 
 func (fm *SmallMap[K, V]) Size() int {
-	return len(fm.data)
+	return len(fm.keys)
 }
 
 func (fm *SmallMap[K, V]) ForEach(f func(key K, value V)) {
-	for i := 0; i < len(fm.data); i++ {
-		f(fm.data[i].key, fm.data[i].value)
+	for i, k := range fm.keys {
+		f(k, fm.values[i])
 	}
 }
 
 func (fm *SmallMap[K, V]) Keys() []K {
-	res := make([]K, len(fm.data))
-	for i := 0; i < len(fm.data); i++ {
-		res[i] = fm.data[i].key
-	}
+	res := make([]K, len(fm.keys))
+	copy(res, fm.keys)
 	return res
 }
 
 func (fm *SmallMap[K, V]) Values() []V {
-	res := make([]V, len(fm.data))
-	for i := 0; i < len(fm.data); i++ {
-		res[i] = fm.data[i].value
-	}
+	res := make([]V, len(fm.values))
+	copy(res, fm.values)
 	return res
 }
 
 func (fm *SmallMap[K, V]) Clear() {
-	var zero entry[K, V]
-	for i := range fm.data {
-		fm.data[i] = zero
+	for i := range fm.keys {
+		var zeroK K
+		var zeroV V
+		fm.keys[i] = zeroK
+		fm.values[i] = zeroV
 	}
-	fm.data = fm.data[:0]
+	fm.keys = fm.keys[:0]
+	fm.values = fm.values[:0]
 }
 
 func (fm *SmallMap[K, V]) Contains(key K) bool {
-	d := fm.data
-	for i := 0; i < len(d); i++ {
-		if d[i].key == key {
+	for _, k := range fm.keys {
+		if k == key {
 			return true
 		}
 	}
