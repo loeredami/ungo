4dd549e22798a2e1df5606de483637cecb7b4e14
loeredami <jskfbjsn018@gmail.com>
2026-03-19 00:34:38 +0100

    small map optimization test

--- small_map.go
+++ small_map.go
@@ -1,158 +1,157 @@
 package ungo
 
 import (
-	"fmt"
+	"unsafe"
 )
 
 type entry[K comparable, V any] struct {
 	key   K
 	value V
-	used  bool
 }
 
 type SmallMap[K comparable, V any] struct {
-	data []entry[K, V]
-	size int
-	mask uintptr
+	data  []entry[K, V]
+	index []uint32
+	mask  uintptr
 }
 
 func NewSmallMap[K comparable, V any](capacity int) *SmallMap[K, V] {
-	pow2 := 16
-	for pow2 < capacity {
+	pow2 := 8
+	for pow2 < capacity*2 {
 		pow2 <<= 1
 	}
 	return &SmallMap[K, V]{
-		data: make([]entry[K, V], pow2),
-		mask: uintptr(pow2 - 1),
+		data:  make([]entry[K, V], 0, capacity),
+		index: make([]uint32, pow2),
+		mask:  uintptr(pow2 - 1),
 	}
 }
 
 func (fm *SmallMap[K, V]) hash(key K) uintptr {
-	var h uintptr = 2166136261
-	s := fmt.Sprintf("%v", key)
-	for i := 0; i < len(s); i++ {
-		h ^= uintptr(s[i])
-		h *= 16777619
-	}
-	return h
+	u := *(*uintptr)(unsafe.Pointer(&key))
+	return u * 0xdeece66d
 }
 
 func (fm *SmallMap[K, V]) Set(key K, value V) {
-	if fm.size*10 >= len(fm.data)*7 {
-		fm.grow()
+	h := fm.hash(key)
+	idx := h & fm.mask
+
+	for {
+		dataIdxPlusOne := fm.index[idx]
+		if dataIdxPlusOne == 0 {
+			break
+		}
+		dataIdx := dataIdxPlusOne - 1
+		if fm.data[dataIdx].key == key {
+			fm.data[dataIdx].value = value
+			return
+		}
+		idx = (idx + 1) & fm.mask
 	}
 
-	h := fm.hash(key)
-	mask := fm.mask
-	idx := h & mask
+	fm.data = append(fm.data, entry[K, V]{key: key, value: value})
+	fm.index[idx] = uint32(len(fm.data))
 
-	for i := uintptr(1); ; i++ {
-		e := &fm.data[idx]
-		if !e.used {
-			*e = entry[K, V]{key: key, value: value, used: true}
-			fm.size++
-			return
-		}
-		if e.key == key {
-			e.value = value
-			return
-		}
-		idx = (idx + i) & mask
+	if len(fm.data)*10 > len(fm.index)*7 {
+		fm.rehashIndex()
 	}
 }
 
 func (fm *SmallMap[K, V]) Get(key K) (V, bool) {
-	if fm.size == 0 {
+	if len(fm.index) == 0 {
 		var zero V
 		return zero, false
 	}
 
-	h := fm.hash(key)
-	mask := fm.mask
-	idx := h & mask
-
-	for i := uintptr(1); ; i++ {
-		e := &fm.data[idx]
-		if !e.used {
+	idx := fm.hash(key) & fm.mask
+	for {
+		dataIdxPlusOne := fm.index[idx]
+		if dataIdxPlusOne == 0 {
 			var zero V
 			return zero, false
 		}
-		if e.key == key {
-			return e.value, true
+		dataIdx := dataIdxPlusOne - 1
+		if fm.data[dataIdx].key == key {
+			return fm.data[dataIdx].value, true
 		}
-		idx = (idx + i) & mask
-		if i > mask {
-			break
-		}
+		idx = (idx + 1) & fm.mask
 	}
-	var zero V
-	return zero, false
+}
+
+func (fm *SmallMap[K, V]) rehashIndex() {
+	newCap := len(fm.index) * 2
+	fm.index = make([]uint32, newCap)
+	fm.mask = uintptr(newCap - 1)
+
+	for i := range fm.data {
+		h := fm.hash(fm.data[i].key)
+		idx := h & fm.mask
+		for fm.index[idx] != 0 {
+			idx = (idx + 1) & fm.mask
+		}
+		fm.index[idx] = uint32(i + 1)
+	}
 }
 
 func (fm *SmallMap[K, V]) Delete(key K) {
 	h := fm.hash(key)
-	mask := fm.mask
-	idx := h & mask
+	idx := h & fm.mask
 
-	for i := uintptr(1); ; i++ {
-		e := &fm.data[idx]
-		if !e.used {
+	for {
+		dataIdxPlusOne := fm.index[idx]
+		if dataIdxPlusOne == 0 {
 			return
 		}
-		if e.key == key {
-			*e = entry[K, V]{}
-			fm.size--
+		dataIdx := dataIdxPlusOne - 1
+		if fm.data[dataIdx].key == key {
+			lastIdx := len(fm.data) - 1
+			if int(dataIdx) != lastIdx {
+				fm.data[dataIdx] = fm.data[lastIdx]
+				fm.updateIndex(fm.data[dataIdx].key, uint32(dataIdx+1))
+			}
+			fm.data[lastIdx] = entry[K, V]{}
+			fm.data = fm.data[:lastIdx]
+
+			fm.rehashIndex()
 			return
 		}
-		idx = (idx + i) & mask
-		if i > mask {
-			break
-		}
+		idx = (idx + 1) & fm.mask
 	}
 }
 
-func (fm *SmallMap[K, V]) grow() {
-	oldData := fm.data
-	newCap := len(oldData) * 2
-	fm.data = make([]entry[K, V], newCap)
-	fm.mask = uintptr(newCap - 1)
-	fm.size = 0
-
-	for i := range oldData {
-		if oldData[i].used {
-			fm.Set(oldData[i].key, oldData[i].value)
+func (fm *SmallMap[K, V]) updateIndex(key K, newIdxPlusOne uint32) {
+	idx := fm.hash(key) & fm.mask
+	for {
+		if fm.data[fm.index[idx]-1].key == key {
+			fm.index[idx] = newIdxPlusOne
+			return
 		}
+		idx = (idx + 1) & fm.mask
 	}
 }
 
 func (fm *SmallMap[K, V]) Size() int {
-	return fm.size
+	return len(fm.data)
 }
 
 func (fm *SmallMap[K, V]) ForEach(f func(key K, value V)) {
 	for i := range fm.data {
-		if fm.data[i].used {
-			f(fm.data[i].key, fm.data[i].value)
-		}
+		f(fm.data[i].key, fm.data[i].value)
 	}
 }
 
 func (fm *SmallMap[K, V]) Keys() []K {
-	res := make([]K, 0, fm.size)
+	res := make([]K, len(fm.data))
 	for i := range fm.data {
-		if fm.data[i].used {
-			res = append(res, fm.data[i].key)
-		}
+		res[i] = fm.data[i].key
 	}
 	return res
 }
 
 func (fm *SmallMap[K, V]) Values() []V {
-	res := make([]V, 0, fm.size)
+	res := make([]V, len(fm.data))
 	for i := range fm.data {
-		if fm.data[i].used {
-			res = append(res, fm.data[i].value)
-		}
+		res[i] = fm.data[i].value
 	}
 	return res
 }
@@ -161,7 +160,10 @@ func (fm *SmallMap[K, V]) Clear() {
 	for i := range fm.data {
 		fm.data[i] = entry[K, V]{}
 	}
-	fm.size = 0
+	fm.data = fm.data[:0]
+	for i := range fm.index {
+		fm.index[i] = 0
+	}
 }
 
 func (fm *SmallMap[K, V]) Contains(key K) bool {
