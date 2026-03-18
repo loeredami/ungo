51af13c6cc7a2eec6ca196d6846e87874d4ead22
loeredami <jskfbjsn018@gmail.com>
2026-03-18 22:54:25 +0100

    experimental small map test

--- small_map.go
+++ small_map.go
@@ -5,17 +5,19 @@ import (
 	"unsafe"
 )
 
-type entry[K comparable, V any] struct {
-	key      K
-	value    V
-	occupied bool
-}
+const (
+	empty    uint8 = 0
+	occupied uint8 = 1
+)
 
 type SmallMap[K comparable, V any] struct {
-	buckets []entry[K, V]
-	size    int
-	mask    uint64
-	seed    maphash.Seed
+	keys     []K
+	values   []V
+	metadata []uint8
+	size     int
+	mask     uint64
+	seed     maphash.Seed
+	hash     maphash.Hash
 }
 
 func NewSmallMap[K comparable, V any](capacity int) *SmallMap[K, V] {
@@ -24,37 +26,43 @@ func NewSmallMap[K comparable, V any](capacity int) *SmallMap[K, V] {
 		realCap <<= 1
 	}
 
-	return &SmallMap[K, V]{
-		buckets: make([]entry[K, V], realCap),
-		mask:    uint64(realCap - 1),
-		seed:    maphash.MakeSeed(),
+	sm := &SmallMap[K, V]{
+		keys:     make([]K, realCap),
+		values:   make([]V, realCap),
+		metadata: make([]uint8, realCap),
+		mask:     uint64(realCap - 1),
+		seed:     maphash.MakeSeed(),
 	}
+	sm.hash.SetSeed(sm.seed)
+	return sm
 }
 
-func (fm *SmallMap[K, V]) hash(key K) uint64 {
-	var h maphash.Hash
-	h.SetSeed(fm.seed)
-
+func (fm *SmallMap[K, V]) getHash(key K) uint64 {
+	fm.hash.Reset()
 	kSize := unsafe.Sizeof(key)
 	ptr := unsafe.Pointer(&key)
 	b := unsafe.Slice((*byte)(ptr), kSize)
 
-	h.Write(b)
-	return h.Sum64()
+	fm.hash.Write(b)
+	return fm.hash.Sum64()
 }
 
 func (fm *SmallMap[K, V]) Set(key K, value V) {
-	idx := fm.hash(key) & fm.mask
+	idx := fm.getHash(key) & fm.mask
+
+	keys := fm.keys
+	meta := fm.metadata
+
 	for {
-		if !fm.buckets[idx].occupied {
-			fm.buckets[idx].key = key
-			fm.buckets[idx].value = value
-			fm.buckets[idx].occupied = true
+		if meta[idx] == empty {
+			keys[idx] = key
+			fm.values[idx] = value
+			meta[idx] = occupied
 			fm.size++
 			return
 		}
-		if fm.buckets[idx].key == key {
-			fm.buckets[idx].value = value
+		if keys[idx] == key {
+			fm.values[idx] = value
 			return
 		}
 		idx = (idx + 1) & fm.mask
@@ -62,27 +70,37 @@ func (fm *SmallMap[K, V]) Set(key K, value V) {
 }
 
 func (fm *SmallMap[K, V]) Get(key K) (V, bool) {
-	idx := fm.hash(key) & fm.mask
+	idx := fm.getHash(key) & fm.mask
+	keys := fm.keys
+	meta := fm.metadata
+
 	for {
-		if !fm.buckets[idx].occupied {
+		if meta[idx] == empty {
 			var zero V
 			return zero, false
 		}
-		if fm.buckets[idx].key == key {
-			return fm.buckets[idx].value, true
+		if keys[idx] == key {
+			return fm.values[idx], true
 		}
 		idx = (idx + 1) & fm.mask
 	}
 }
 
 func (fm *SmallMap[K, V]) Delete(key K) {
-	idx := fm.hash(key) & fm.mask
+	idx := fm.getHash(key) & fm.mask
+	keys := fm.keys
+	meta := fm.metadata
+
 	for {
-		if !fm.buckets[idx].occupied {
+		if meta[idx] == empty {
 			return
 		}
-		if fm.buckets[idx].key == key {
-			fm.buckets[idx].occupied = false
+		if keys[idx] == key {
+			meta[idx] = empty
+			var zeroK K
+			var zeroV V
+			keys[idx] = zeroK
+			fm.values[idx] = zeroV
 			fm.size--
 			fm.rehashCluster(idx)
 			return
@@ -95,11 +113,18 @@ func (fm *SmallMap[K, V]) rehashCluster(hole uint64) {
 	i := hole
 	for {
 		i = (i + 1) & fm.mask
-		if !fm.buckets[i].occupied {
+		if fm.metadata[i] == empty {
 			return
 		}
-		k, v := fm.buckets[i].key, fm.buckets[i].value
-		fm.buckets[i].occupied = false
+
+		k, v := fm.keys[i], fm.values[i]
+		fm.metadata[i] = empty
+
+		var zeroK K
+		var zeroV V
+		fm.keys[i] = zeroK
+		fm.values[i] = zeroV
+
 		fm.size--
 		fm.Set(k, v)
 	}
@@ -110,40 +135,47 @@ func (fm *SmallMap[K, V]) Size() int {
 }
 
 func (fm *SmallMap[K, V]) ForEach(f func(key K, value V)) {
-	for i := range fm.buckets {
-		if fm.buckets[i].occupied {
-			f(fm.buckets[i].key, fm.buckets[i].value)
+	meta := fm.metadata
+	keys := fm.keys
+	vals := fm.values
+	for i := range meta {
+		if meta[i] == occupied {
+			f(keys[i], vals[i])
 		}
 	}
 }
 
 func (fm *SmallMap[K, V]) Keys() []K {
-	keys := make([]K, 0, fm.size)
-	for i := range fm.buckets {
-		if fm.buckets[i].occupied {
-			keys = append(keys, fm.buckets[i].key)
+	res := make([]K, 0, fm.size)
+	meta := fm.metadata
+	keys := fm.keys
+	for i := range meta {
+		if meta[i] == occupied {
+			res = append(res, keys[i])
 		}
 	}
-	return keys
+	return res
 }
 
 func (fm *SmallMap[K, V]) Values() []V {
-	vals := make([]V, 0, fm.size)
-	for i := range fm.buckets {
-		if fm.buckets[i].occupied {
-			vals = append(vals, fm.buckets[i].value)
+	res := make([]V, 0, fm.size)
+	meta := fm.metadata
+	vals := fm.values
+	for i := range meta {
+		if meta[i] == occupied {
+			res = append(res, vals[i])
 		}
 	}
-	return vals
+	return res
 }
 
 func (fm *SmallMap[K, V]) Clear() {
-	for i := range fm.buckets {
-		fm.buckets[i].occupied = false
-		var zeroK K
-		var zeroV V
-		fm.buckets[i].key = zeroK
-		fm.buckets[i].value = zeroV
+	var zeroK K
+	var zeroV V
+	for i := range fm.metadata {
+		fm.metadata[i] = empty
+		fm.keys[i] = zeroK
+		fm.values[i] = zeroV
 	}
 	fm.size = 0
 }
