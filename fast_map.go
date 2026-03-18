076e3c81ff0f67e7b8f0ee8fcc7b2b2fc0311a87
loeredami <jskfbjsn018@gmail.com>
2026-03-18 22:35:23 +0100

    fast map, but this time compiles

--- fast_map.go
+++ fast_map.go
@@ -1,182 +1,87 @@
 package ungo
 
-/*
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-
-typedef struct {
-    void* key;
-    void* value;
-    uint8_t occupied;
-} Entry;
-
-typedef struct {
-    Entry* entries;
-    size_t capacity;
-    size_t size;
-    size_t key_size;
-    size_t val_size;
-} CMap;
-
-// FNV-1a hash for raw bytes
-static uint64_t hash_bytes(const uint8_t* data, size_t len) {
-    uint64_t hash = 14695981039346656037ULL;
-    for (size_t i = 0; i < len; i++) {
-        hash ^= data[i];
-        hash *= 1099511628211ULL;
-    }
-    return hash;
-}
-
-CMap* create_map(size_t cap, size_t k_size, size_t v_size) {
-    CMap* m = malloc(sizeof(CMap));
-    m->capacity = cap;
-    m->size = 0;
-    m->key_size = k_size;
-    m->val_size = v_size;
-    m->entries = calloc(cap, sizeof(Entry));
-    return m;
-}
-
-void free_map(CMap* m) {
-    for (size_t i = 0; i < m->capacity; i++) {
-        if (m->entries[i].occupied) {
-            free(m->entries[i].key);
-            free(m->entries[i].value);
-        }
-    }
-    free(m->entries);
-    free(m);
-}
-
-void set(CMap* m, void* key, void* value) {
-    uint64_t h = hash_bytes((uint8_t*)key, m->key_size) % m->capacity;
-    while (m->entries[h].occupied) {
-        if (memcmp(m->entries[h].key, key, m->key_size) == 0) {
-            memcpy(m->entries[h].value, value, m->val_size);
-            return;
-        }
-        h = (h + 1) % m->capacity;
-    }
-    m->entries[h].key = malloc(m->key_size);
-    m->entries[h].value = malloc(m->val_size);
-    memcpy(m->entries[h].key, key, m->key_size);
-    memcpy(m->entries[h].value, value, m->val_size);
-    m->entries[h].occupied = 1;
-    m->size++;
-}
-
-int get(CMap* m, void* key, void* out_val) {
-    uint64_t h = hash_bytes((uint8_t*)key, m->key_size) % m->capacity;
-    uint64_t start = h;
-    while (m->entries[h].occupied) {
-        if (memcmp(m->entries[h].key, key, m->key_size) == 0) {
-            memcpy(out_val, m->entries[h].value, m->val_size);
-            return 1;
-        }
-        h = (h + 1) % m->capacity;
-        if (h == start) break;
-    }
-    return 0;
-}
-
-void delete_key(CMap* m, void* key) {
-    uint64_t h = hash_bytes((uint8_t*)key, m->key_size) % m->capacity;
-    while (m->entries[h].occupied) {
-        if (memcmp(m->entries[h].key, key, m->key_size) == 0) {
-            free(m->entries[h].key);
-            free(m->entries[h].value);
-            m->entries[h].occupied = 0;
-            m->size--;
-            // Linear probing re-insertion logic omitted for brevity in this generic version
-            return;
-        }
-        h = (h + 1) % m->capacity;
-    }
-}
-*/
-import "C"
-import (
-	"unsafe"
-)
-
 type FastMap[K comparable, V any] struct {
-	ptr *C.CMap
-}
-
-func NewFastMap[K comparable, V any](capacity int) *FastMap[K, V] {
-	var k K
-	var v V
-	return &FastMap[K, V]{
-		ptr: C.create_map(C.size_t(capacity), C.size_t(unsafe.Sizeof(k)), C.size_t(unsafe.Sizeof(v))),
-	}
+	indexes []K
+	values  []V
 }
 
 func (fm *FastMap[K, V]) Set(key K, value V) {
-	C.set(fm.ptr, unsafe.Pointer(&key), unsafe.Pointer(&value))
+	for i := 0; i < len(fm.indexes); i++ {
+		if fm.indexes[i] == key {
+			fm.values[i] = value
+			return
+		}
+	}
+	fm.indexes = append(fm.indexes, key)
+	fm.values = append(fm.values, value)
 }
 
 func (fm *FastMap[K, V]) Get(key K) (V, bool) {
-	var val V
-	found := C.get(fm.ptr, unsafe.Pointer(&key), unsafe.Pointer(&val))
-	if found == 1 {
-		return val, true
+	idx := fm.indexes
+	for i := 0; i < len(idx); i++ {
+		if idx[i] == key {
+			return fm.values[i], true
+		}
 	}
-	return val, false
+	var zero V
+	return zero, false
 }
 
 func (fm *FastMap[K, V]) Delete(key K) {
-	C.delete_key(fm.ptr, unsafe.Pointer(&key))
-}
+	for i := 0; i < len(fm.indexes); i++ {
+		if fm.indexes[i] == key {
+			lastIdx := len(fm.indexes) - 1
 
-func (fm *FastMap[K, V]) Contains(key K) bool {
-	_, found := fm.Get(key)
-	return found
-}
+			copy(fm.indexes[i:], fm.indexes[i+1:])
+			copy(fm.values[i:], fm.values[i+1:])
 
-func (fm *FastMap[K, V]) Size() int {
-	return int(fm.ptr.size)
-}
+			var zeroK K
+			var zeroV V
+			fm.indexes[lastIdx] = zeroK
+			fm.values[lastIdx] = zeroV
 
-func (fm *FastMap[K, V]) Clear() {
-	cap := fm.ptr.capacity
-	kSize := fm.ptr.key_size
-	vSize := fm.ptr.val_size
-	C.free_map(fm.ptr)
-	fm.ptr = C.create_map(cap, kSize, vSize)
-}
-
-func (fm *FastMap[K, V]) ForEach(f func(key K, value V)) {
-	entries := unsafe.Slice(fm.ptr.entries, fm.ptr.capacity)
-	for i := 0; i < int(fm.ptr.capacity); i++ {
-		if entries[i].occupied == 1 {
-			k := *(*K)(entries[i].key)
-			v := *(*V)(entries[i].value)
-			f(k, v)
+			fm.indexes = fm.indexes[:lastIdx]
+			fm.values = fm.values[:lastIdx]
+			return
 		}
 	}
 }
 
+func (fm *FastMap[K, V]) Size() int {
+	return len(fm.indexes)
+}
+
+func (fm *FastMap[K, V]) ForEach(f func(key K, value V)) {
+	for i := 0; i < len(fm.indexes); i++ {
+		f(fm.indexes[i], fm.values[i])
+	}
+}
+
 func (fm *FastMap[K, V]) Keys() []K {
-	keys := make([]K, 0, int(fm.ptr.size))
-	fm.ForEach(func(k K, v V) {
-		keys = append(keys, k)
-	})
-	return keys
+	return fm.indexes
 }
 
 func (fm *FastMap[K, V]) Values() []V {
-	values := make([]V, 0, int(fm.ptr.size))
-	fm.ForEach(func(k K, v V) {
-		values = append(values, v)
-	})
-	return values
+	return fm.values
 }
 
-func (fm *FastMap[K, V]) Close() {
-	if fm.ptr != nil {
-		C.free_map(fm.ptr)
-		fm.ptr = nil
+func (fm *FastMap[K, V]) Clear() {
+	var zeroK K
+	var zeroV V
+	for i := range fm.indexes {
+		fm.indexes[i] = zeroK
+		fm.values[i] = zeroV
 	}
+	fm.indexes = fm.indexes[:0]
+	fm.values = fm.values[:0]
+}
+
+func (fm *FastMap[K, V]) Contains(key K) bool {
+	idx := fm.indexes
+	for i := 0; i < len(idx); i++ {
+		if idx[i] == key {
+			return true
+		}
+	}
+	return false
 }
