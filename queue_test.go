c96439844d17a7bcbc9ed35aa5aafe21fbf9d061
loeredami <jskfbjsn018@gmail.com>
2026-03-18 16:16:49 +0100

    Fixed other oopsie in queue_test.go

--- queue_test.go
+++ queue_test.go
@@ -6,18 +6,18 @@ func TestQueue(t *testing.T) {
 	q := NewQueue[int]()
 	q.Push(42)
 	if q.Pop().OrElse(0) != 42 {
-		t.Errorf("expected 42, got %d", q.Pop())
+		t.Errorf("expected 42")
 	}
 	if q.Pop().OrElse(0) != 0 {
-		t.Errorf("expected 0, got %d", q.Pop())
+		t.Errorf("expected 0")
 	}
 
 	q.Push(42)
 	q.Push(43)
 	if q.Pop().OrElse(0) != 42 {
-		t.Errorf("expected 42, got %d", q.Pop())
+		t.Errorf("expected 42")
 	}
 	if q.Pop().OrElse(0) != 43 {
-		t.Errorf("expected 43, got %d", q.Pop())
+		t.Errorf("expected 43")
 	}
 }
